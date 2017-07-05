0dc4b4681963712a51d7035148fb5ea56b2ead37
Frédéric Hardy <frederic.hardy@mageekbox.net>
2017-07-05 08:16:12 +0200

    Remove useless code.

--- src/coordinate/recipient/functor.php
+++ src/coordinate/recipient/functor.php
@@ -1,22 +1,13 @@
 <?php namespace estvoyage\ticTacToe\coordinate\recipient;
 
-use estvoyage\ticTacToe\coordinate;
+use estvoyage\ticTacToe\{ coordinate, block };
 
-class functor
+class functor extends block\functor
 	implements
 		coordinate\recipient
 {
-	private
-		$callable
-	;
-
-	function __construct(callable $callable)
-	{
-		$this->callable = $callable;
-	}
-
 	function lineAndColumnOfTicTacToeSymbolIs(int $line, int $column) :void
 	{
-		call_user_func_array($this->callable, func_get_args());
+		parent::blockArgumentsAre($line, $column);
 	}
 }
--- src/symbol/container/recipient/functor.php
+++ src/symbol/container/recipient/functor.php
@@ -1,22 +1,13 @@
 <?php namespace estvoyage\ticTacToe\symbol\container\recipient;
 
-use estvoyage\ticTacToe\symbol;
+use estvoyage\ticTacToe\{ symbol, block };
 
-class functor
+class functor extends block\functor
 	implements
 		symbol\container\recipient
 {
-	private
-		$callable
-	;
-
-	function __construct(callable $callable)
-	{
-		$this->callable = $callable;
-	}
-
 	function ticTacToeSymbolContainerIs(symbol\container $container) :void
 	{
-		call_user_func_array($this->callable, func_get_args());
+		parent::blockArgumentsAre($container);
 	}
 }
