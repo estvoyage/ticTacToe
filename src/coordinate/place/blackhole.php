27d3911906a9d3637f8aa163babe2527f1a0d7ec
Frédéric Hardy <frederic.hardy@mageekbox.net>
2017-07-25 13:47:40 +0200

    Add blackhole for `coordinate\place`.

---
+++ src/coordinate/place/blackhole.php
@@ -0,0 +1,12 @@
+<?php namespace estvoyage\ticTacToe\coordinate\place;
+
+use estvoyage\ticTacToe\{ coordinate, ninteger };
+
+class blackhole
+	implements
+		coordinate\place
+{
+	function recipientOfNIntegerGreaterThanZeroIs(ninteger\recipient $recipient) :void
+	{
+	}
+}
